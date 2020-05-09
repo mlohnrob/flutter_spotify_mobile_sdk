fcb7b362c14187f55b2ce130ff138fd44ca10063
August Emmery-Funch <32144571+augu1352@users.noreply.github.com>
2020-05-09 17:28:24 +0200

    stuff

--- android/src/main/java/com/mlohnrob/FlutterSpotifySDK/spotify_mobile_sdk/SpotifyMobileSdkPlugin.java
+++ android/src/main/java/com/mlohnrob/FlutterSpotifySDK/spotify_mobile_sdk/SpotifyMobileSdkPlugin.java
@@ -12,18 +12,24 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 public class SpotifyMobileSdkPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "spotify_mobile_sdk");
+    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(),
+        "spotify_mobile_sdk");
     channel.setMethodCallHandler(new SpotifyMobileSdkPlugin());
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+  // This static function is optional and equivalent to onAttachedToEngine. It
+  // supports the old
   // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
+  // plugin registration via this function while apps migrate to use the new
+  // Android APIs
   // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
   //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+  // It is encouraged to share logic between onAttachedToEngine and registerWith
+  // to keep
+  // them functionally equivalent. Only one of onAttachedToEngine or registerWith
+  // will be called
+  // depending on the user's project. onAttachedToEngine or registerWith must both
+  // be defined
   // in the same class.
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "spotify_mobile_sdk");
@@ -33,7 +39,7 @@ public class SpotifyMobileSdkPlugin implements FlutterPlugin, MethodCallHandler 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+      result.success("Motherfucking ANDROID: " + android.os.Build.VERSION.RELEASE);
     } else {
       result.notImplemented();
     }
