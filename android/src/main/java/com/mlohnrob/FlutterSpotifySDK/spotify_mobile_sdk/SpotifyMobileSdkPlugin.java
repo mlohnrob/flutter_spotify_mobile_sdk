83d0896a15dd337f1b8e3672ce98c706d8cfd151
August Emmery-Funch <32144571+augu1352@users.noreply.github.com>
2020-07-09 23:27:54 +0200

    bool -> Boolean

--- android/src/main/java/com/mlohnrob/FlutterSpotifySDK/spotify_mobile_sdk/SpotifyMobileSdkPlugin.java
+++ android/src/main/java/com/mlohnrob/FlutterSpotifySDK/spotify_mobile_sdk/SpotifyMobileSdkPlugin.java
@@ -136,7 +136,7 @@ public class SpotifyMobileSdkPlugin implements FlutterPlugin, MethodCallHandler 
     }
   }
 
-  private bool pause(@NonNull Result result) {
+  private Boolean pause(@NonNull Result result) {
     try {
       mSpotifyAppRemote.getPlayerApi().pause().setResultCallback(result.success(true));
     } catch (final Exception e) {
